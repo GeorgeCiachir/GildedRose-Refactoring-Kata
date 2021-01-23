c885819315509c1c61babfb0476889800ade91ba
GeorgeCiachir <george.ciachir@yahoo.com>
2021-01-23 18:23:38 +0200

    add tests for normal items

---
+++ Java/src/test/java/com/gildedrose/DexterityVestTest.java
@@ -0,0 +1,72 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DexterityVestTest extends SellInTest {
+
+    private static final int DEGRADE_TWICE = 2;
+
+    @Test
+    public void qualityDegradesWithNormalStepGivenSellInNotPassed() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 20;
+        Item actual = new Item("+5 Dexterity Vest", initialSellInDays, initialQuality);
+        Item[] items = new Item[]{actual};
+
+        //when
+        int daysPassed = 9;
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < daysPassed; i++) {
+            app.updateQuality();
+        }
+
+        //then
+        assertThat(actual.quality).isEqualTo(initialQuality - daysPassed);
+    }
+
+    @Test
+    public void qualityDegradesTwiceAsFastGivenSellInPassed() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 20;
+        Item actual = new Item("+5 Dexterity Vest", initialSellInDays, initialQuality);
+        Item[] items = new Item[]{actual};
+
+        //when
+        int daysAfterSellInPassed = 4;
+        // don't overshoot this, so the quality won't go to zero
+        assertThat(daysAfterSellInPassed).isLessThanOrEqualTo((initialQuality - initialSellInDays) / DEGRADE_TWICE - 1);
+        int totalDaysPassed = initialSellInDays + daysAfterSellInPassed;
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < totalDaysPassed; i++) {
+            app.updateQuality();
+        }
+
+        //then
+        System.out.println(actual.quality);
+        assertThat(actual.quality).isEqualTo(initialQuality - initialSellInDays - DEGRADE_TWICE * daysAfterSellInPassed);
+    }
+
+    @Test
+    public void qualityNeverGoesBelowZero() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 20;
+        Item actual = new Item("+5 Dexterity Vest", initialSellInDays, initialQuality);
+        Item[] items = new Item[]{actual};
+
+        //when
+        int daysToPassAfterSellToReachZeroQuality = (initialQuality - initialSellInDays) / DEGRADE_TWICE;
+        int totalDaysPassed = initialSellInDays + daysToPassAfterSellToReachZeroQuality + 1;
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < totalDaysPassed; i++) {
+            app.updateQuality();
+        }
+
+        //then
+        assertThat(actual.quality).isEqualTo(0);
+    }
+}
