67527dd3f5a445bbd9f9136831ffecd3866f1f87
GeorgeCiachir <george.ciachir@yahoo.com>
2021-01-25 16:33:50 +0200

    update tests

--- Java/src/test/java/com/gildedrose/TextTests.java
+++ Java/src/test/java/com/gildedrose/TextTests.java
@@ -19,19 +19,17 @@ public class TextTests {
     public void testEvolution() {
         //given
         Item[] items = new Item[]{
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                // to be added after refactoring
-                // new Item("Conjured Mana Cake", 3, 6)
+                new Item("Conjured Mana Cake", 3, 6),
+                new Item("Conjured Mana Cake", 10, 50)
         };
-
 
         //when
         GildedRose app = new GildedRose(items);
