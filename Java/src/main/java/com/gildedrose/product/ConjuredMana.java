b4f124cf0e1badf37772d5a3996b076f4bb09704
GeorgeCiachir <george.ciachir@yahoo.com>
2021-01-25 11:26:36 +0200

    add conjured mana test and implement product update

--- Java/src/main/java/com/gildedrose/product/ConjuredMana.java
+++ Java/src/main/java/com/gildedrose/product/ConjuredMana.java
@@ -1,8 +1,16 @@
 package com.gildedrose.product;
 
 import com.gildedrose.Item;
+import com.gildedrose.product.error.Validator;
+
+import java.util.List;
+
+import static com.gildedrose.product.error.Validators.defaultValidators;
 
 public class ConjuredMana implements Product {
+
+    private static final int NORMAL_DEGRADATION = 2;
+    private static final int DOUBLE_DEGRADATION = 2 * NORMAL_DEGRADATION;
 
     private final Item item;
 
@@ -17,11 +25,70 @@ public class ConjuredMana implements Product {
     }
 
     private void updateQuality() {
+        if (expired()) {
+            return;
+        }
 
+        if (shouldDegradeTwice()) {
+            degradeTwice();
+            return;
+        }
+
+        if (shouldDegradeNormal()) {
+            degradeNormal();
+            return;
+        }
+
+        degradeByOne();
+    }
+
+    private boolean expired() {
+        return item.quality == 0;
+    }
+
+    private boolean shouldDegradeTwice() {
+        return sellInPassed()
+                && canDegradeTwice();
+    }
+
+    private boolean canDegradeTwice() {
+        return item.quality >= DOUBLE_DEGRADATION;
+    }
+
+    private void degradeTwice() {
+        item.quality = item.quality - DOUBLE_DEGRADATION;
+    }
+
+    private boolean shouldDegradeNormal() {
+        return canDegradeNormal();
+    }
+
+    private boolean canDegradeNormal() {
+        return item.quality >= NORMAL_DEGRADATION;
+    }
+
+    private void degradeNormal() {
+        item.quality = item.quality - NORMAL_DEGRADATION;
+    }
+
+    private void degradeByOne() {
+        item.quality--;
+    }
+
+    private boolean withinSellIn() {
+        return item.sellIn > 0;
+    }
+
+    private boolean sellInPassed() {
+        return !withinSellIn();
     }
 
     private void updateSellIn() {
         item.sellIn--;
+    }
+
+    public static List<Validator> getValidators() {
+        return defaultValidators();
     }
 
     @Override
---
+++ Java/src/test/java/com/gildedrose/product/ConjuredManaTest.java
@@ -0,0 +1,135 @@
+package com.gildedrose.product;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConjuredManaTest extends BaseTest {
+
+    private static final int DEGRADE_NORMAL = 2;
+    private static final int DEGRADE_TWICE = 2 * DEGRADE_NORMAL;
+    private static final int MIN_QUALITY = 0;
+
+    @Test
+    public void degradesNormalBeforeSellIn() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 30;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        int daysPassed = initialSellInDays - 1;
+        for (int i = 0; i < daysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        int degradation = DEGRADE_NORMAL * daysPassed;
+        assertThat(conjured.quality).isEqualTo(initialQuality - degradation);
+    }
+
+    @Test
+    public void degradesNormalThenStepOneBeforeSellIn() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 3;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        // 1 of degradation 2
+        // 1 of degradation 1
+        int daysPassed = 3;
+        for (int i = 0; i < daysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        assertThat(conjured.quality).isEqualTo(MIN_QUALITY);
+    }
+
+    @Test
+    public void degradesNormalBeforeSellInThenTwiceAsFastThenNormal() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 31;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        // 10 of degradation 2
+        // 2 of degradation 4
+        // 1 of degradation 2
+        int totalDaysPassed = 13;
+        for (int i = 0; i < totalDaysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        int normalDegradation = initialSellInDays * DEGRADE_NORMAL;
+        int doubleDegradation = 2 * DEGRADE_TWICE;
+        int oneStepDegradation = 1 * DEGRADE_NORMAL;
+        assertThat(conjured.quality).isEqualTo(initialQuality - normalDegradation - doubleDegradation - oneStepDegradation);
+        assertThat(conjured.quality).isEqualTo(1);
+    }
+
+    @Test
+    public void degradesNormalBeforeSellInThenTwiceAsFastThenNormalThenStepOne() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 31;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        // 10 of degradation 2
+        // 2 of degradation 4
+        // 1 of degradation 2
+        // 1 of degradation 1
+        int totalDaysPassed = 14;
+        for (int i = 0; i < totalDaysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        assertThat(conjured.quality).isEqualTo(MIN_QUALITY);
+    }
+
+    @Test
+    public void qualityNeverGoesBelowZero() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 10;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        int totalDaysPassed = initialSellInDays + 1;
+        for (int i = 0; i < totalDaysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        assertThat(conjured.quality).isEqualTo(MIN_QUALITY);
+    }
+
+    @Test
+    public void testSellInDecrease() {
+        //given
+        int initialSellInDays = 10;
+        int initialQuality = 20;
+        Item conjured = new Item("Conjured Mana Cake", initialSellInDays, initialQuality);
+        Product conjuredProduct = new ProductBuilder(ProductType.CONJURED_MANA).buildFrom(conjured);
+
+        //when
+        int totalDaysPassed = 60;
+        for (int i = 0; i < totalDaysPassed; i++) {
+            conjuredProduct.update();
+        }
+
+        //then
+        assertThat(conjured.sellIn).isEqualTo(initialSellInDays - totalDaysPassed);
+    }
+}
